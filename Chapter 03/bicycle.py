72c82f406c64d28da3273134d10ba5c0f1fa7ad4
Wenzurk <wenzurk_ma@163.com>
2018-02-03 14:15:40 +0800

    20180203

---
+++ Chapter 03/bicycle.py
@@ -0,0 +1,17 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: Wenzurk
+# Created on: 2018/2/3
+
+bicycle = ["trek", "cannondale", "redline", "specialized"]
+print(bicycle)
+
+print(bicycle[0])
+
+print(bicycle[1])
+print(bicycle[3])
+
+print(bicycle[-1])
+
+message = "My first bicycle is " + bicycle[0].title() + "."
+print(message)
