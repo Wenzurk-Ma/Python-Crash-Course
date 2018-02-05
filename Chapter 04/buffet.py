b6150f9cc3943a8a498e8d1f43004bc0a473a073
Wenzurk <wenzurk_ma@163.com>
2018-02-05 16:46:12 +0800

    20180204

---
+++ Chapter 04/buffet.py
@@ -0,0 +1,14 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: Wenzurk
+# Created on: 2018/2/5
+
+foods = ('pizza', 'falafel', 'carrot cake', 'steak', 'salad')
+for food in foods:
+    print(food)
+
+# foods[1] = 'bee'
+
+foods = ('pizza', 'falafel', 'carrot cake', 'ice cream', 'ham')
+for food in foods:
+    print(food)
---
+++ Chapter 04/dimensions.py
@@ -0,0 +1,19 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: Wenzurk
+# Created on: 2018/2/5
+
+dimensions = (200, 50)
+# print(dimensions[0])
+# print(dimensions[1])
+
+# dimensions[0]=250
+
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 100)
+print("\nModified dimensions:")
+for dimension in dimensions:
+    print(dimension)
