e539723b67cfbec49bac8ad5ab518a061b150419
Wenzurk <wenzurk_ma@163.com>
2018-02-05 11:53:54 +0800

    20180204

---
+++ Chapter 04/magicians.py
@@ -0,0 +1,15 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: Wenzurk
+# Created on: 2018/2/5
+
+# magicians = ['alice', 'david', 'carolina']
+# for magician in magicians:
+#     # print(magician)
+#     print(magician.title() + ", that was a great trick!")
+# print("I can't wait to see your next trick, " + magician.title() + ".\n")
+#
+# print("Thank you, everyone. That was a great magic show!")
+
+# message = "Hello"
+#     print(message)
