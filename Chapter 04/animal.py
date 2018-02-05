2a0175237e8402f255f439c7cea9295b3ffa655b
Wenzurk <wenzurk_ma@163.com>
2018-02-05 13:20:43 +0800

    20180204

---
+++ Chapter 04/animal.py
@@ -0,0 +1,11 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: Wenzurk
+# Created on: 2018/2/5
+
+animals = ['dog', 'cat', 'pig']
+for animal in animals:
+    # print(animal)
+    print("A " + animal + " would make a great pet.")
+
+print("All of these animals would make a great pet!")
---
+++ Chapter 04/pizza.py
@@ -0,0 +1,11 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: Wenzurk
+# Created on: 2018/2/5
+
+pizza_list = ['meat', 'meat2', 'meat3', 'meat4']
+for pizza in pizza_list:
+    # print(pizza)
+    print("I like " + pizza + " pizza.")
+
+print("I really love pizza.")
