03a66aae7ab66f6e227bfe191af0c76edc4bd9b2
Wenzurk <wenzurk_ma@163.com>
2018-02-06 19:38:15 +0800

    20180205

---
+++ Chapter 05/fruits.py
@@ -0,0 +1,17 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: Wenzurk
+# Created on: 2018/2/6
+
+favorite_fruits = ['apple', 'pear', 'banana', 'orange', 'blueberry']
+
+if 'apple' in favorite_fruits:
+    print("You really like apple!")
+if 'people' in favorite_fruits:
+    print("You really like people!")
+if 'banana' in favorite_fruits:
+    print("You really like banana!")
+if 'woman' in favorite_fruits:
+    print("You really like woman!")
+if 'blueberry' in favorite_fruits:
+    print("You really like blueberry!")
